5cf5916db1aac0ec9981833130cf17c7f9a73efb
Liam Armstrong <ltarmstrong94@gmail.com>
2023-10-27 02:19:01 -0700

    added queries for new viz

--- pandas/RFM_calculation.py
+++ pandas/RFM_calculation.py
@@ -23,15 +23,21 @@ data = pd.read_sql_query(query, engine)
 
 # Calculate quartiles (percentiles) using pandas
 # The quartiles represent the distribution of the Recency, Frequency, and Monetary values across your customer base.
-quartiles = data.quantile([0.25, 0.50, 0.75])
+quartiles = data.quantile([0.10, 0.25, 0.50, 0.75, 0.90])
 
 # Define segmentation criteria based on quartiles
 R25, R50, R75 = quartiles['Recency'][0.25], quartiles['Recency'][0.50], quartiles['Recency'][0.75]
 F25, F50, F75 = quartiles['Frequency'][0.25], quartiles['Frequency'][0.50], quartiles['Frequency'][0.75]
 M25, M50, M75 = quartiles['Monetary'][0.25], quartiles['Monetary'][0.50], quartiles['Monetary'][0.75]
 
+# Calculate 10th and 90th percentiles for Monetary
+M10 = quartiles['Monetary'][0.10]
+M90 = quartiles['Monetary'][0.90]
+
+
 # Create a new column in your DataFrame to store the segment for each customer
 data['Segment'] = 'Low'  # Initialize all customers to 'Low' segment
+data['MonetarySegment'] = 'Low-Value Customer'  # Initialize all customers to 'Low-Value Customer' segment
 
 # Apply conditional logic to assign customers to different segments based on their RFM values
 data.loc[data['Recency'] <= R25, 'Segment'] = 'Low'
@@ -46,7 +52,13 @@ data.loc[data['Monetary'] <= M25, 'Segment'] = 'Low'
 data.loc[(data['Monetary'] > M25) & (data['Monetary'] <= M50), 'Segment'] = 'Medium'
 data.loc[data['Monetary'] > M50, 'Segment'] = 'High'
 
+# Create a new column for the segment based on the new monetary calculation
+data.loc[data['Monetary'] >= M90, 'MonetarySegment'] = 'High-Value Customer'
+data.loc[(data['Monetary'] > M10) & (data['Monetary'] < M90), 'MonetarySegment'] = 'Medium-Value Customer'
+
 # Insert the segmented data into the RFM_Segmented table in MySQL
 data.to_sql('RFM_Segmented', con=engine, if_exists='replace', index=False)
 
 print("Data has been inserted into RFM_Segmented table.")
+
+
--- queries/sales_trend_queries.sql
+++ queries/sales_trend_queries.sql
@@ -11,6 +11,7 @@ GROUP BY YEAR(InvoiceDate), MONTH(InvoiceDate)
 ORDER BY Year, Month;
 
 -- Identify days with sales 2 standard deviations above the mean (days with abnormally high sales)
+CREATE VIEW DailySalesView AS
 WITH DailySales AS (
     SELECT 
         DATE(InvoiceDate) AS Date,
@@ -21,6 +22,9 @@ WITH DailySales AS (
 SELECT Date, TotalSales
 FROM DailySales
 WHERE TotalSales > (SELECT AVG(TotalSales) + 2 * STD(TotalSales) FROM DailySales);
+
+
+
 
 -- Unusual purchase behavior:
 -- Example: customers buying more than 1000 units of a product in one purchase.)
@@ -62,4 +66,92 @@ WHERE Quantity <= 0
 GROUP BY StockCode
 ORDER BY OutOfStockCount DESC;
 
+-- Monthly / Seasonal Sales Data:
+DROP TABLE SeasonalSales;
+CREATE TABLE IF NOT EXISTS SeasonalSales (
+    Year INT,
+    Month INT,
+    MonthlySales DECIMAL(12, 2)
+);
+
+INSERT INTO SeasonalSales (Year, Month, MonthlySales)
+SELECT
+    YEAR(ecom.InvoiceDate) AS Year,
+    MONTH(ecom.InvoiceDate) AS Month,
+    SUM(rfm.TotalPurchaseValue) AS MonthlySales
+FROM
+    ecommerce_data ecom
+JOIN
+    RFM_Aggregated rfm
+ON
+    ecom.CustomerID = rfm.CustomerID
+WHERE
+    ecom.CustomerID <> 0 -- Exclude rows where CustomerID is 0
+GROUP BY
+    Year, Month
+ORDER BY
+    Year, Month;
+
+-- Monthly Sales calculation : 
+SELECT
+    YEAR(ecom.InvoiceDate) AS Year,
+    MONTH(ecom.InvoiceDate) AS Month,
+    SUM(rfm.TotalPurchaseValue) AS MonthlySales
+FROM
+    ecommerce_data ecom
+JOIN
+    RFM_Aggregated rfm
+ON
+    ecom.CustomerID = rfm.CustomerID
+GROUP BY
+    Year, Month
+ORDER BY
+    Year, Month;
+    
+SELECT * FROM SeasonalSales;
+
+-- Top Selling Products Table Creation 
+CREATE TABLE TopSellingProducts (
+    StockCode VARCHAR(255),
+    Description VARCHAR(255),
+    TotalQuantitySold INT
+);
+INSERT INTO TopSellingProducts (StockCode, Description, TotalQuantitySold)
+SELECT
+    StockCode,
+    Description,
+    SUM(Quantity) AS TotalQuantitySold
+FROM
+    ecommerce_data
+GROUP BY
+    StockCode, Description
+ORDER BY
+    TotalQuantitySold DESC
+LIMIT 10; -- Change the limit to display more or fewer products
+
+SELECT * FROM TopSellingProducts;
+
+-- Create a table for Sales Growth Data
+CREATE TABLE IF NOT EXISTS SalesGrowthData (
+    Year INT,
+    Month INT,
+    MonthlySales DECIMAL(12, 2),
+    PreviousMonthSales DECIMAL(12, 2),
+    SalesGrowthRate DECIMAL(12, 2)
+);
+
+-- Insert the results of the SQL query into the table and round the SalesGrowthRate
+INSERT INTO SalesGrowthData (Year, Month, MonthlySales, PreviousMonthSales, SalesGrowthRate)
+SELECT
+    Year,
+    Month,
+    MonthlySales,
+    LAG(MonthlySales) OVER (ORDER BY Year, Month) AS PreviousMonthSales,
+    ROUND((MonthlySales - LAG(MonthlySales) OVER (ORDER BY Year, Month)) / LAG(MonthlySales) OVER (ORDER BY Year, Month), 2) AS SalesGrowthRate
+FROM
+    SeasonalSales
+ORDER BY
+    Year, Month;
+    
+SELECT * FROM SalesGrowthData;
 
